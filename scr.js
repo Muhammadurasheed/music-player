72c2c76a6622ffd7764dd5e25f4447c88d95a62f
Muhammadrasheed <yekinirasheed2002@gmail.com>
2022-09-04 12:06:25 +0100

    pause and play functionality added

--- scr.js
+++ scr.js
@@ -39,7 +39,30 @@ const songs = [
   },
 ];
 
-
+let songIndex = 0;
+const prevSong = () => {
+    songIndex--;
+    if(songIndex <= -1) {
+      songIndex = 0
+      prevBtn.classList.add('prev-btn-disabled')
+    } else {
+      nextBtn.classList.remove('next-btn-disabled')
+      loadSongs(songs[songIndex])
+      playSong()
+    }
+    
+}
+const nextSong = () => {
+    songIndex++;
+    if(songIndex >= songs.length) {
+      songIndex = songs.length-1;
+      nextBtn.classList.add('next-btn-disabled')
+    } else {
+      prevBtn.classList.remove('prev-btn-disabled')
+      loadSongs(songs[songIndex])
+      playSong()
+    }
+}
 
 // Update DOM
 const loadSongs = song => {
@@ -72,6 +95,40 @@ const controlPlay = () => {
 
 // update progress bar & time
 
+const updateProgressBar = event => {
+  const { currentTime, duration } = event.srcElement
+// Update the progress bar width
+const progressPercent = (currentTime/duration) * 100
+progress.style.width = `${progressPercent}%`;
+// Computing song duration
+const durationInMin = Math.floor(duration / 60)
+let durationInSec = Math.floor(duration % 60);
+if(durationInSec < 10) {
+  durationInSec = `0${durationInSec}`
+}
+// Delay switching duration element to avoid NaN
+if(durationInSec) {
+  durationElem.textContent = `${durationInMin}:${durationInSec}`
+}
+
+// start timing
+const currentTimeInMin = Math.floor(currentTime / 60)
+let currentTimeInSec = Math.floor(currentTime % 60);
+if(currentTimeInSec < 10) {
+  currentTimeInSec = `0${currentTimeInSec}`
+}
+// Delay switching currentTime element to avoid NaN
+if(currentTimeInSec) {
+  currentTimeElem.textContent = `${currentTimeInMin}:${currentTimeInSec}`
+}
+}
+
+  function setProgressBar (event) {
+  const width = this.clientWidth
+  const clickedPart  = event.offsetX  
+  const { duration } = music;
+  music.currentTime = ((clickedPart/width) * duration);
+}
 
 // Event Listeners
 
